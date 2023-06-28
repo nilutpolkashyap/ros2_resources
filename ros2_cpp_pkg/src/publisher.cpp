30030830b44f1ffe4ff7a5442437de1563363925
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2023-06-28 23:54:00 +0530

    adding comments

--- ros2_cpp_pkg/src/publisher.cpp
+++ ros2_cpp_pkg/src/publisher.cpp
@@ -1,27 +1,26 @@
 #include <chrono>
 #include <functional> 
-
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "rclcpp/rclcpp.hpp"   //ROS 2 client library for C++
+#include "std_msgs/msg/string.hpp"   //import built-in string message type from ROS data types
 
 using namespace std::chrono_literals;
 
-class HelloWorldPubNode : public rclcpp::Node
+class HelloWorldPubNode : public rclcpp::Node  // creating a new class  inheriting from rclcpp Node class
 {
   public: 
-    HelloWorldPubNode() : Node("hello_world_pub_node") 
+    HelloWorldPubNode() : Node("hello_world_pub_node")   // ==> node name
     {
       publisher_ = this->create_publisher<std_msgs::msg::String>("hello_world", 10);
       timer_ = this->create_wall_timer(1s, std::bind(&HelloWorldPubNode::publish_hello_world, this));
     }
 
   private:
-    void publish_hello_world()
+    void publish_hello_world()    //callback function ==> call this function from within your code – here as a callback in a timer
     {
-      auto message = std_msgs::msg::String();
+      auto message = std_msgs::msg::String();   //create String message object
       message.data = "Hello World  " + std::to_string(counter_);
-      publisher_->publish(message);
-      counter_++;
+      publisher_->publish(message);   // publish() function from the publisher object to actually publish on the topic
+      counter_++;    //incrementing counter variable
     }
 
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
@@ -32,9 +31,9 @@ class HelloWorldPubNode : public rclcpp::Node
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<HelloWorldPubNode>());
-  rclcpp::shutdown();
+  rclcpp::init(argc, argv);    // Initialize the rclcpp library
+  rclcpp::spin(std::make_shared<HelloWorldPubNode>());    // allowing the ros node to keep running
+  rclcpp::shutdown();    // shutdown rclpy instances
   
   return 0;
 }
--- ros2_cpp_pkg/src/subscriber.cpp
+++ ros2_cpp_pkg/src/subscriber.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+#include "rclcpp/rclcpp.hpp"   //ROS 2 client library for C++
+#include "std_msgs/msg/string.hpp"    //import built-in string message type from ROS data types
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp" 
 
-
-class HelloWorldSubNode : public rclcpp::Node
-{
+class HelloWorldSubNode : public rclcpp::Node   //creating a new class  inheriting from rclcpp Node class
+{ 
   public: 
-    HelloWorldSubNode() : Node("hello_world_sub_node") 
+    HelloWorldSubNode() : Node("hello_world_sub_node")   ## ==> node name
 		{
 			subscription_ = this->create_subscription<std_msgs::msg::String>(
 				"hello_world", 10, std::bind(&HelloWorldSubNode::sub_callback, this, std::placeholders::_1)
@@ -25,9 +24,9 @@ class HelloWorldSubNode : public rclcpp::Node
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<HelloWorldSubNode>());
-  rclcpp::shutdown();
+  rclcpp::init(argc, argv);   // Initialize the rclcpp library
+  rclcpp::spin(std::make_shared<HelloWorldSubNode>());   // allowing the ros node to keep running
+  rclcpp::shutdown();    // shutdown rclpy instances
   
   return 0;
 }
